1a7d103d3abe0b5af9038cf2174d2e5a77634d7b
Yuriy (Юрий) <lit-uriy@sibna.ru>
2020-12-08 08:49:52 +0500

    Сделал проект-клон.

---
+++ t2_blink_hal/f070/Src/main.c
@@ -0,0 +1,177 @@
+/**
+  ******************************************************************************
+  * @file    Templates/Src/main.c 
+  * @author  MCD Application Team
+  * @brief   Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2016 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/** @addtogroup STM32F0xx_HAL_Examples
+  * @{
+  */
+
+/** @addtogroup Templates
+  * @{
+  */
+
+/* Private typedef -----------------------------------------------------------*/
+/* Private define ------------------------------------------------------------*/
+/* Private macro -------------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+static GPIO_InitTypeDef  GPIO_InitStruct;
+
+/* Private function prototypes -----------------------------------------------*/
+static void SystemClock_Config(void);
+static void Error_Handler(void);
+
+/* Private functions ---------------------------------------------------------*/
+
+/**
+  * @brief  Main program
+  * @param  None
+  * @retval None
+  */
+int main(void)
+{
+
+  /* STM32F0xx HAL library initialization:
+       - Configure the Flash prefetch
+       - Systick timer is configured by default as source of time base, but user 
+         can eventually implement his proper time base source (a general purpose 
+         timer for example or other time source), keeping in mind that Time base 
+         duration should be kept 1ms since PPP_TIMEOUT_VALUEs are defined and 
+         handled in milliseconds basis.
+       - Low Level Initialization
+     */
+  HAL_Init();
+
+  /* Configure the system clock to have a system clock = 48 Mhz */
+  SystemClock_Config();
+  
+  /* -1- Разрешим тактирование порта A (светодиод сидит на порте A) */
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+
+  /* -2- Настраиваем ножку контроллера, к которой подключен светодиод: Выход, push-pull */
+  GPIO_InitStruct.Mode  = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull  = GPIO_PULLUP; // <-- нахрена, если и так push-pull?
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH; // <-- нахрена, если эсли это маргалка герцовой частоты?
+  GPIO_InitStruct.Pin = LED2_PIN;
+
+  HAL_GPIO_Init(LED2_GPIO_PORT, &GPIO_InitStruct);
+
+  /* -3- Бесконечно переключаем светодиод */
+  while (1)
+  {
+    HAL_GPIO_TogglePin(LED2_GPIO_PORT, LED2_PIN);
+    /* задержка 100 ms */
+    HAL_Delay(100);
+  }
+}
+
+/**
+  * @brief  System Clock Configuration
+  *         The system Clock is configured as follow : 
+  *            System Clock source            = PLL (HSI)
+  *            SYSCLK(Hz)                     = 48000000
+  *            HCLK(Hz)                       = 48000000
+  *            AHB Prescaler                  = 1
+  *            APB1 Prescaler                 = 1
+  *            HSI Frequency(Hz)              = 8000000
+  *            PREDIV                         = 1
+  *            PLLMUL                         = 6
+  *            Flash Latency(WS)              = 1
+  * @param  None
+  * @retval None
+  */
+static void SystemClock_Config(void)
+{
+  RCC_ClkInitTypeDef RCC_ClkInitStruct;
+  RCC_OscInitTypeDef RCC_OscInitStruct;
+  
+  /* На Nucleo не запаян кварц для HSE, поэтому активируем PLL с HSI в качестве источника */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState          = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue  = RCC_HSICALIBRATION_DEFAULT;
+
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;          // PLL: включен
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;  // Источник тактов для PLL: HSI
+  RCC_OscInitStruct.PLL.PREDIV = RCC_PREDIV_DIV1;       // Предделитель PLL = 1
+  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL6;          // Умножение частоты на выходе PLL = 6
+
+  // Применяем конфигурацию генератора
+  //    Здесь идёт ожидание RCC_FLAG_HSIRDY и RCC_FLAG_PLLRDY
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct)!= HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /* Установим system clock source = PLL и настроим предделители HCLK, PCLK1 */
+  RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1);  // какой тип тактирования настраиваем
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;     // Источник тактирования для SYSCLK (HSI, HSE или PLL для STM32F070RB)
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;            // Предделитель AHB (HCLK, ядро) = 1
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;             // Предделитель APB (PCLK1, периферия) = 1
+
+  // Применяем конфигурацию тактовых сигналов и циклов ожидания записи/стирания Flash-памяти
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1)!= HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @param  None
+  * @retval None
+  */
+static void Error_Handler(void)
+{
+  /* User may add here some code to deal with this error */
+  while(1)
+  {
+  }
+}
+
+#ifdef  USE_FULL_ASSERT
+
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t* file, uint32_t line)
+{ 
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+
+  /* Infinite loop */
+  while (1)
+  {
+  }
+}
+#endif
+
+/**
+  * @}
+  */
+
+/**
+  * @}
+  */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
